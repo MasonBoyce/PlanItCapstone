0855f4e62947cd4369f98c6d10b5da9d8772dfbc
Garrett Gilliom <garrettgilliom@gmail.com>
2022-11-09 15:10:35 -0600

    created playground class for initial development

---
+++ PlanIt/Playground.swift
@@ -0,0 +1,20 @@
+//
+//  Playground.swift
+//  PlanIt
+//
+//  Created by Garrett Gilliom on 11/9/22.
+//
+
+import Foundation
+
+class Playground {
+    var testDummy: Int
+    
+    init(testVar : Int) {
+        self.testDummy = testVar
+    }
+    
+    func testPrint(){
+        print(self.testDummy)
+    }
+}
