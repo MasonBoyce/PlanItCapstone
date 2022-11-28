af9eaed3e8b75a625e07f8d45fb9b816aef2856b
Michael-Xu <meemichael01@gmail.com>
2022-11-28 10:13:38 -0600

    Storyboard Integration
    Storyboard integrated for main view controller

--- PlanIt/Welcome/WelcomeCoordinator.swift
+++ PlanIt/Welcome/WelcomeCoordinator.swift
@@ -20,16 +20,39 @@ class WelcomeCoordinator: WelcomeCoordinatorProtocol, Coordinator {
     
     //Initializes view controller model and connects them.
     //Pushes the view controller to the top of the screen
+    //Old View Controller
+
+//    func start() {
+//        let viewController: WelcomeViewController = WelcomeViewController()
+//        let model: WelcomeModel = WelcomeModel()
+    
+//        viewController.model = model
+////        model.viewController = viewController
+//        model.coordinator = self
+//    navigationController.pushViewController(viewController, animated: true)
+//    }
+    
     func start() {
-        let viewController: WelcomeViewController = WelcomeViewController()
-        let model: WelcomeModel = WelcomeModel()
-        
-        viewController.model = model
-        model.viewController = viewController
-        model.coordinator = self
-        
-        navigationController.pushViewController(viewController, animated: true)
+         // The first time this coordinator started, is to launch login page.
+    goToLoginPage()
     }
+    
+    let storyboard = UIStoryboard.init(name: "Main", bundle: .main)
+    func goToLoginPage(){
+         // Instantiate LoginViewController
+         let loginViewController = storyboard.instantiateViewController(withIdentifier: "LoginViewController")
+         // Instantiate LoginViewModel
+//         let loginViewModel = LoginViewModel.init()
+         // Set the Coordinator to the ViewModel
+//         loginViewModel.appCoordinator = self
+         // Set the ViewModel to ViewController
+//         loginViewController.viewModel = loginViewModel
+         // Push it.
+        navigationController.pushViewController(loginViewController, animated: true)
+    }
+    
+    
+    
     //Intilize mapCoordinator as a child coordinator then starts it
     func goToMap() {
         let mapCoordinator = MapCoordinator(navigationController: navigationController)
--- PlanIt/Welcome/WelcomeViewController.swift
+++ PlanIt/Welcome/WelcomeViewController.swift
@@ -8,69 +8,125 @@
 import Foundation
 import UIKit
 
-class WelcomeViewController: UIViewController, WelcomeViewControllerProtocol {
+class WelcomeViewController: UIViewController {
+//class WelcomeViewController: UIViewController, WelcomeViewControllerProtocol {
     var model: WelcomeModelProtocol?
     
-    //MARK: View Elements
-    lazy var viewContainter: UIView = {
-        let view = UIView()
-        view.translatesAutoresizingMaskIntoConstraints = false
-        view.backgroundColor = .blue
-        return view
-    }()
-    
-    lazy var welcomeLabel: UILabel = {
-        let label = UILabel()
-        label.text = "WELCOME TO PLANIT"
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
-    
-    lazy var button: UIButton =  {
-        let button = UIButton()
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.setTitle("Click Button", for: .normal)
-        button.addTarget(self, action: #selector(doSomething), for: .touchUpInside)
-        button.backgroundColor = .yellow
-        return button
-    }()
-    
-    //MARK: Functions
     override func viewDidLoad() {
-        view.backgroundColor = .red
-        setUpView()
         super.viewDidLoad()
+//        view.backgroundColor = .link
     }
     
-    @objc func doSomething() {
-        model?.doSomething(button: button)
-    }
-    
-    required init?(coder: NSCoder) {
-        super.init(coder: coder)
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    init() {
-        super.init(nibName: nil, bundle: nil)
-    }
-    
-    func setUpView() {
-        view.addSubview(viewContainter)
-        viewContainter.addSubview(welcomeLabel)
-        viewContainter.addSubview(button)
+    @IBAction func didTapButton(){
         
-        viewContainter.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
-        viewContainter.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10).isActive = true
-        viewContainter.trailingAnchor.constraint(equalTo: view.trailingAnchor,constant: -10).isActive = true
-        viewContainter.bottomAnchor.constraint(equalTo: view.bottomAnchor,constant: -100).isActive = true
-        
-        welcomeLabel.topAnchor.constraint(equalTo: viewContainter.topAnchor, constant: 10).isActive  =  true
-        welcomeLabel.centerXAnchor.constraint(equalTo: viewContainter.centerXAnchor).isActive = true
-        welcomeLabel.heightAnchor.constraint(equalToConstant: 100).isActive = true
-        
-        button.topAnchor.constraint(equalTo: welcomeLabel.bottomAnchor,constant: -20).isActive = true
-        button.centerXAnchor.constraint(equalTo: welcomeLabel.centerXAnchor).isActive = true
-        button.bottomAnchor.constraint(equalTo: viewContainter.bottomAnchor).isActive = true
     }
+    
+//    view.layer.shadowColor = UIColor.black.cgColor
+//    view.layer.shadowOpacity = 0.2
+//    view.layer.shadowOffset = CGSize(width: 4, height: 4)
+//    view.layer.shadowRadius = 5.0
+//
+//    // Set masksToBounds to false, otherwise the shadow
+//    // will be clipped and will not appear outside of
+//    // the view’s frame
+//    view.layer.masksToBounds = false
+//
+//    @IBDesignable extension UIView {
+//        @IBInspectable var shadowRadius: CGFloat {
+//            get { return layer.shadowRadius }
+//            set { layer.shadowRadius = newValue }
+//        }
+//
+//        @IBInspectable var shadowOpacity: CGFloat {
+//            get { return CGFloat(layer.shadowOpacity) }
+//            set { layer.shadowOpacity = Float(newValue) }
+//        }
+//
+//        @IBInspectable var shadowOffset: CGSize {
+//            get { return layer.shadowOffset }
+//            set { layer.shadowOffset = newValue }
+//        }
+//
+//        @IBInspectable var shadowColor: UIColor? {
+//            get {
+//                guard let cgColor = layer.shadowColor else {
+//                    return nil
+//                }
+//                return UIColor(cgColor: cgColor)
+//            }
+//            set { layer.shadowColor = newValue?.cgColor }
+//        }
+//    }
+//
+//    //MARK: View Elements
+//    lazy var viewContainter: UIView = {
+//        let view = UIView()
+//        view.translatesAutoresizingMaskIntoConstraints = false
+//        view.backgroundColor = .white
+//        return view
+//    }()
+//
+//    lazy var welcomeLabel: UILabel = {
+//        let label = UILabel()
+//        label.text = "WELCOME TO PLANIT"
+//        label.translatesAutoresizingMaskIntoConstraints = false
+//        return label
+//    }()
+//
+//    lazy var button: UIButton =  {
+//        let button = UIButton()
+//        button.translatesAutoresizingMaskIntoConstraints = false
+//        button.setTitle("Login with Apple", for: .normal)
+//        button.addTarget(self, action: #selector(doSomething), for: .touchUpInside)
+//        button.layer.cornerRadius = button.bounds.size.height/2
+//        button.backgroundColor = .black
+//        return button
+//    }()
+//
+////    struct SignInWithAppleButton {
+////        init(SignInWithAppleButton.Label, onRequest: (ASAuthorizationAppleIDRequest) -> Void, onCompletion: ((Result<ASAuthorization, Error>) -> Void))}
+////
+////    struct SignInWithAppleButton.Style{
+////
+////    }
+//
+//    //MARK: Functions
+//    override func viewDidLoad() {
+//        view.backgroundColor = .black
+////        view.backgroundImage: UIImage = (UIImage(named: "hello"))!
+//        setUpView()
+//        super.viewDidLoad()
+//    }
+//
+//    @objc func doSomething() {
+//        model?.doSomething(button: button)
+//    }
+//
+//    required init?(coder: NSCoder) {
+//        super.init(coder: coder)
+//        fatalError("init(coder:) has not been implemented")
+//    }
+//
+//    init() {
+//        super.init(nibName: nil, bundle: nil)
+//    }
+//
+//    func setUpView() {
+//        view.addSubview(viewContainter)
+//        viewContainter.addSubview(welcomeLabel)
+//        viewContainter.addSubview(button)
+//
+//        viewContainter.topAnchor.constraint(equalTo: view.topAnchor, constant: 0).isActive = true
+//        viewContainter.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10).isActive = true
+//        viewContainter.trailingAnchor.constraint(equalTo: view.trailingAnchor,constant: -10).isActive = true
+//        viewContainter.bottomAnchor.constraint(equalTo: view.bottomAnchor,constant: 0).isActive = true
+//
+//        welcomeLabel.topAnchor.constraint(equalTo: viewContainter.topAnchor, constant: 10).isActive  =  true
+//        welcomeLabel.centerXAnchor.constraint(equalTo: viewContainter.centerXAnchor).isActive = true
+//        welcomeLabel.heightAnchor.constraint(equalToConstant: 200).isActive = true
+//
+//        button.topAnchor.constraint(equalTo: welcomeLabel.bottomAnchor,constant: 100).isActive = true
+//        button.centerXAnchor.constraint(equalTo: welcomeLabel.centerXAnchor).isActive = true
+//        button.bottomAnchor.constraint(equalTo: viewContainter.bottomAnchor,constant: -100).isActive = true
+//    }
 }
