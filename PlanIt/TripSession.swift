aea995271015e92215098355abda6dce85e2aa53
Garrett Gilliom <garrettgilliom@gmail.com>
2023-02-13 17:25:08 -0600

    Added TripSession class with some variables, methods in place

---
+++ PlanIt/TripSession.swift
@@ -0,0 +1,70 @@
+//
+//  TripSession.swift
+//  PlanIt
+//
+//  Created by Garrett Gilliom on 2/12/23.
+//
+
+import Foundation
+import UIKit
+import MapKit
+
+class TripSession {
+    
+    var venues: [Venue]
+    var optimal_venue_route: [Int]
+    var cost_matrix: [[Double]]
+    // var info_matrix: [[?????]] // to store entire route objs in matrix
+    var id_to_venue_dict: [Int: Venue]
+    var all_venue_permutations: [[Int]]
+    var all_venue_pairs: [(Int, Int)]
+    
+    init(newVenues: [Venue]) {
+        venues = newVenues
+        
+        let num_venues = venues.count
+        cost_matrix = Array(repeating: Array(repeating: -1.0, count: num_venues), count: num_venues) // info_matrix???
+        
+        id_to_venue_dict = [Int: Venue]()
+        for (index, venue) in venues.enumerated(){
+            id_to_venue_dict[index] = venue
+        }
+        
+        all_venue_permutations = [[Int]]()
+        all_venue_pairs = [(Int, Int)]()
+        optimal_venue_route = [Int]()
+    }
+    
+    func get_route_cost(start_id: Int, end_id: Int) -> Double {
+        var cost = cost_matrix[start_id][end_id]
+        if cost < 0 {
+            // set_route_info(start_id: Int, end_id: Int) // calculate route between start_id/end_id, assign to info_matrix
+            // cost = info_matrix[start_id, end_id].cost // ??? grab cost from proper info_matrix cell
+            set_route_cost(start_id: start_id, end_id: end_id, cost: cost)
+            cost = cost_matrix[start_id][end_id]
+        }
+        
+        return cost
+    }
+    
+    func set_route_cost(start_id: Int, end_id: Int, cost: Double) { // change to route_info to store entire route objs?
+        cost_matrix[start_id][end_id] = cost
+    }
+    
+    /*
+    func get_route_info(start_id: Int, end_id: Int) -> ????? { // only if info_matrix is used
+        var route = info_matrix[start_id][end_id]
+        if (route has not been set yet) {
+            set_route_info(start_id: start_id, end_id: end_id)
+            route = info_matrix[start_id][end_id]
+        }
+        return route
+    }
+     
+    func set_route_info(start_id: Int, end_id: Int) { // only if info_matrix is used
+        var route = calculate_route(start_id: Int, end_id: Int)
+        info_matrix[start_id][end_id] = route
+    }
+     */
+    
+}
