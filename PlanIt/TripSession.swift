6cc379c3df50efcdcb4ab15509053894280a7d92
Garrett Gilliom <garrettgilliom@gmail.com>
2023-02-13 17:42:28 -0600

    Added get/set methods for venue permutations

--- PlanIt/TripSession.swift
+++ PlanIt/TripSession.swift
@@ -12,6 +12,7 @@ import MapKit
 class TripSession {
     
     var venues: [Venue]
+    var venue_ids: [Int]
     var optimal_venue_route: [Int]
     var cost_matrix: [[Double]]
     // var info_matrix: [[?????]] // to store entire route objs in matrix
@@ -26,8 +27,10 @@ class TripSession {
         cost_matrix = Array(repeating: Array(repeating: -1.0, count: num_venues), count: num_venues) // info_matrix???
         
         id_to_venue_dict = [Int: Venue]()
+        venue_ids = [Int]()
         for (index, venue) in venues.enumerated(){
             id_to_venue_dict[index] = venue
+            venue_ids.append(index)
         }
         
         all_venue_permutations = [[Int]]()
@@ -66,5 +69,44 @@ class TripSession {
         info_matrix[start_id][end_id] = route
     }
      */
+    
+    func get_venue_permutations() -> [[Int]] {
+        if all_venue_permutations.count == 0 {
+            set_venue_permutations(k: venues.count, venues: venue_ids)
+        }
+        return all_venue_permutations
+    }
+    
+    // With help from https://en.wikipedia.org/wiki/Heap%27s_algorithm (Heap's Algorithm)
+    // k is length of venues list; venues list is original order of routes
+    func set_venue_permutations(k: Int, venues: [Int]){
+        //print(" ")
+        var coords = venues
+        if k == 1 {
+            all_venue_permutations.append(coords)
+        } else {
+            // Generate permutations with k-th unaltered
+            // Initially k = length(A)
+            set_venue_permutations(k: k - 1, venues: coords)
+            var temp: Int
+            //print("k:", k)
+            // Generate permutations for k-th swapped with each k-1 initial
+            for i in (0 ... (k - 2)) {
+                //print("i:", i)
+                if k % 2 == 0 { // swap venues[i], venues[i+1]
+                    temp = coords[i]
+                    coords[i] = coords[k - 1]
+                    coords[k - 1] = temp
+                } else {
+                    temp = coords[0]
+                    coords[0] = coords[k - 1]
+                    coords[k - 1] = temp
+                }
+                set_venue_permutations(k: k - 1, venues: coords)
+            }
+        }
+    }
+    
+    
     
 }
