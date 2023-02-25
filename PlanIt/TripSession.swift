577097188041921d9e2ed1642ef50f6e71a988e2
Garrett Gilliom <garrettgilliom@gmail.com>
2023-02-25 15:55:17 -0600

    Finished TripSession getters/setters

--- PlanIt/TripSession.swift
+++ PlanIt/TripSession.swift
@@ -15,7 +15,7 @@ class TripSession {
     var venue_ids: [Int]
     var optimal_venue_route: [Int]
     var cost_matrix: [[Double]]
-    // var info_matrix: [[?????]] // to store entire route objs in matrix
+    var route_matrix: [[MKRoute]] // to store entire route objs in matrix
     var id_to_venue_dict: [Int: Venue]
     var all_venue_permutations: [[Int]]
     var all_venue_pairs: [(Int, Int)]
@@ -24,7 +24,10 @@ class TripSession {
         venues = newVenues
         
         let num_venues = venues.count
+        
         cost_matrix = Array(repeating: Array(repeating: -1.0, count: num_venues), count: num_venues) // info_matrix???
+        let nil_route = MKRoute()
+        route_matrix = Array(repeating: Array(repeating: nil_route, count: num_venues), count: num_venues) // info_matrix???
         
         id_to_venue_dict = [Int: Venue]()
         venue_ids = [Int]()
@@ -38,38 +41,77 @@ class TripSession {
         optimal_venue_route = [Int]()
     }
     
-    func get_route_cost(start_id: Int, end_id: Int) -> Double {
-        var cost = cost_matrix[start_id][end_id]
-        if cost < 0 {
-            // set_route_info(start_id: Int, end_id: Int) // calculate route between start_id/end_id, assign to info_matrix
-            // cost = info_matrix[start_id, end_id].cost // ??? grab cost from proper info_matrix cell
-            set_route_cost(start_id: start_id, end_id: end_id, cost: cost)
-            cost = cost_matrix[start_id][end_id]
+    func get_route_cost(source_id: Int, destination_id: Int) -> Double {
+        var route = route_matrix[source_id][destination_id]
+        var name_check = route.name
+        
+        if name_check == "" {
+            route = route_matrix[destination_id][source_id]
+            name_check = route.name
+            
+            if name_check == "" { // Check if cost exists for reverse path; likely is similar cost
+                calculate_route(source_id: source_id, destination_id: destination_id)
+                route = route_matrix[source_id][destination_id]
+            }
+            
         }
+        
+        let cost = route.expectedTravelTime
         
         return cost
     }
     
-    func set_route_cost(start_id: Int, end_id: Int, cost: Double) { // change to route_info to store entire route objs?
-        cost_matrix[start_id][end_id] = cost
-    }
-    
-    /*
-    func get_route_info(start_id: Int, end_id: Int) -> ????? { // only if info_matrix is used
-        var route = info_matrix[start_id][end_id]
-        if (route has not been set yet) {
-            set_route_info(start_id: start_id, end_id: end_id)
-            route = info_matrix[start_id][end_id]
+    func get_route(source_id: Int, destination_id: Int) -> MKRoute { // only if info_matrix is used
+        var route = route_matrix[source_id][destination_id]
+        var name_check = route.name
+        
+        if name_check == "" {
+            calculate_route(source_id: source_id, destination_id: destination_id)
+            route = route_matrix[source_id][destination_id]
         }
+        
         return route
     }
-     
-    func set_route_info(start_id: Int, end_id: Int) { // only if info_matrix is used
-        var route = calculate_route(start_id: Int, end_id: Int)
-        info_matrix[start_id][end_id] = route
-    }
-     */
     
+    func calculate_route(source_id: Int, destination_id: Int) {
+        let sourceVenue = id_to_venue_dict[source_id]
+        let destinationVenue = id_to_venue_dict[destination_id]
+        
+        let sourceLocation = CLLocationCoordinate2D(latitude: sourceVenue?.latitude ?? -1.0, longitude: sourceVenue?.longitude ?? -1.0)
+        let destinationLocation = CLLocationCoordinate2D(latitude: destinationVenue?.latitude ?? -1.0, longitude: destinationVenue?.longitude ?? -1.0)
+        
+        let sourcePlaceMark = MKPlacemark(coordinate: sourceLocation, addressDictionary: nil)
+        let destinationPlaceMark = MKPlacemark(coordinate: destinationLocation, addressDictionary: nil)
+        
+        let sourceMapItem = MKMapItem(placemark: sourcePlaceMark)
+        let destinationItem = MKMapItem(placemark: destinationPlaceMark)
+        
+        let directionRequest = MKDirections.Request()
+        directionRequest.source = sourceMapItem
+        directionRequest.destination = destinationItem
+        directionRequest.transportType = .walking
+        
+        let direction = MKDirections(request: directionRequest)
+        
+        direction.calculate { (response, error) in
+            guard let response = response else {
+                if let error = error {
+                    print("ERROR FOUND : \(error.localizedDescription)")
+                }
+                return
+            }
+            
+            let route = response.routes[0]
+            
+            // Assign source->destination to route_matrix[source][destination]
+            self.route_matrix[source_id][destination_id] = route
+            //        let rect = route.polyline.boundingMapRect
+            //
+            //        self.mapView.setRegion(MKCoordinateRegion(rect), animated: true)
+        }
+        
+    }
+     
     func get_venue_permutations() -> [[Int]] {
         if all_venue_permutations.count == 0 {
             set_venue_permutations(k: venues.count, venues: venue_ids)
@@ -106,7 +148,5 @@ class TripSession {
             }
         }
     }
-    
-    
     
 }
