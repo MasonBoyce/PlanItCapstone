6415eac3bcdd2250e138b369fc8715aff1475e15
Garrett Gilliom <garrettgilliom@gmail.com>
2023-02-26 12:20:52 -0600

    Cleaned up AppCoordinator start() fn

--- PlanIt/AppCoordinator.swift
+++ PlanIt/AppCoordinator.swift
@@ -21,11 +21,6 @@ class AppCoordinator: Coordinator {
     func start() {
         goToWelcome()
         
-        var venue_ex = Venue(name: "ex", id: "0", rating: 4, price: "$", is_closed: false, distance: 1.1, address: "2531 Palmer Ave.", latitude: 2.2, longitude: 3.3)
-        
-        var trip_ex = TripSession(newVenues: [venue_ex])
-        
-        
     }
     //Itilizes welcome as a child coordinator and go to it
     func goToWelcome() {
